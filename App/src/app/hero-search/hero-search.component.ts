f7cde4daf4e55c875130bc0ece58df07a92c0e34
YauheniMel <207melnik@gmail.com>
2022-01-29 10:57:51 +0300

    fix: Add style

---
+++ App/src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.scss']
+})
+export class HeroSearchComponent implements OnInit {
+
+  @Input() name:string = '';
+
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHero(term)),
+    )
+  }
+
+  search(term:string): void {
+    this.searchTerms.next(term);
+  }
+
+}
